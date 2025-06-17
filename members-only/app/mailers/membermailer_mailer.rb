16135c6a1aeb1b8530813ee6f03da33108344e28
petar101 <53666612+petar101@users.noreply.github.com>
2025-06-18 09:47:21 +1000

    dd

---
+++ members-only/app/mailers/membermailer_mailer.rb
@@ -0,0 +1,2 @@
+class MembermailerMailer < ApplicationMailer
+end
---
+++ members-only/test/mailers/previews/membermailer_mailer_preview.rb
@@ -0,0 +1,3 @@
+# Preview all emails at http://localhost:3000/rails/mailers/membermailer_mailer
+class MembermailerMailerPreview < ActionMailer::Preview
+end
