6c180b02ef0241d65e874d97f9fd9673df988d5e
petar101 <53666612+petar101@users.noreply.github.com>
2025-06-24 08:52:19 +1000

    dd

--- members-only/app/mailers/user_mailer.rb
+++ members-only/app/mailers/user_mailer.rb
@@ -4,7 +4,10 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url  = "http://example.com/login"
-    mail(to: @user.email, subject: "Welcome to My Awesome Site")
+    mail(
+    to: email_address_with_name(@user.email, @user.name),
+    subject: "Welcome to My Awesome Site"
+  )
   end
 
   def welcome
